37b72f953e823b7aea96cead42ae8d918c745f10
rizkakhrnnsa <khrnnsa.rizka@gmail.com>
2022-06-05 19:26:42 +0700

    Add project repository.

---
+++ 1. Memulai Pemrograman Dengan Kotlin/TestKotlin/src/main/kotlin/Test.kt
@@ -0,0 +1,17 @@
+// ini contoh single line komentar pada main function
+fun main() {
+    println("Hello Kotlin!")
+//    helloName("Rizka Khairunnisa")
+
+    val name = "Alfian"
+
+    print("Hello my name is ")
+    println(name)
+    print(if (true) "Always true" else "Always false")
+}
+
+// ini contoh multi line komentar
+/*
+fun helloName(name: String) {
+    println("Hello, $name")
+}*/
