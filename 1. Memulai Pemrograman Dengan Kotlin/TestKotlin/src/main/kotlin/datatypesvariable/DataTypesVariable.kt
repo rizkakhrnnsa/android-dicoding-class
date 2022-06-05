a94b16cf03e15654daace34af369e401f738780f
rizkakhrnnsa <khrnnsa.rizka@gmail.com>
2022-06-05 20:47:20 +0700

    Add data types & variables.

---
+++ 1. Memulai Pemrograman Dengan Kotlin/TestKotlin/src/main/kotlin/datatypesvariable/DataTypesVariable.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var company: String = "Dicoding"
+    company = "Dicoding Academy"
+    println(company)
+
+    val firstWord = "Dicoding"
+    val lastWord = "Academy"
+    println(firstWord + " " + lastWord)
+
+    val valueA: Int = 10
+    val valueB = 20
+    print(valueA + valueB)
+}
