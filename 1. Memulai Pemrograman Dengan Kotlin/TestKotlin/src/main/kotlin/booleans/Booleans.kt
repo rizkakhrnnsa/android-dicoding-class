6b93ded8129deda4f7b5a8f8eec7bcc0995dea09
rizkakhrnnsa <khrnnsa.rizka@gmail.com>
2022-06-18 21:27:47 +0700

    Add booleans.

---
+++ 1. Memulai Pemrograman Dengan Kotlin/TestKotlin/src/main/kotlin/booleans/Booleans.kt
@@ -0,0 +1,40 @@
+package booleans
+
+fun main() {
+    operatorAnd()
+    operatorOr()
+    operatorNot()
+}
+
+fun operatorAnd() {
+    val officeOpen = 7
+    val officeClosed = 16
+    val now = 20
+
+    val isOpen = (now >= officeOpen) && (now <= officeClosed)
+
+    println("Office is open : $isOpen")
+}
+
+fun operatorOr() {
+    val officeOpen = 7
+    val officeClosed = 16
+    val now = 20
+
+    val isClosed = (now < officeOpen) || (now > officeClosed)
+
+    println("Office is closed : $isClosed")
+}
+
+fun operatorNot() {
+    val officeOpen = 7
+    val now = 10
+    val isOpen = now > officeOpen
+
+    if (!isOpen) {
+        println("Office is closed")
+    } else {
+        println("Office is open")
+    }
+}
+
