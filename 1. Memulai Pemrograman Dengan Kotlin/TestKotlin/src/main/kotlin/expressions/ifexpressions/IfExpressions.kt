1d0a54f163de21944a9ce173e57d073218a84ae7
rizkakhrnnsa <khrnnsa.rizka@gmail.com>
2022-06-18 20:39:57 +0700

    Add if expressions.

---
+++ 1. Memulai Pemrograman Dengan Kotlin/TestKotlin/src/main/kotlin/expressions/ifexpressions/IfExpressions.kt
@@ -0,0 +1,29 @@
+package expressions.ifexpressions
+
+fun main() {
+    condition()
+}
+
+fun condition() {
+    val openHours = 20
+    val now = 7
+    val office = if (now > openHours) {
+        "office already open"
+    } else {
+         "Office is closed"
+    }
+
+//    println(office)
+
+    val openHours2 = 5
+    val now2 = 10
+    val office2 = if (now2 > 19) {
+        "Office already open"
+    } else if (now2 == openHours2) {
+        "Wait a minute, office will be open"
+    } else {
+        "Office is closed"
+    }
+
+    println(office2)
+}
