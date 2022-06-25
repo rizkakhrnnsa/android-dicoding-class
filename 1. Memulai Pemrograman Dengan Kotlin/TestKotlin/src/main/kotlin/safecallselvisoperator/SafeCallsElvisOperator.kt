ba081071ebdf4265663c03cde82bfcb60dbb4f9b
rizkakhrnnsa <khrnnsa.rizka@gmail.com>
2022-06-25 20:47:18 +0700

    Add safe calls and elvis operator.

---
+++ 1. Memulai Pemrograman Dengan Kotlin/TestKotlin/src/main/kotlin/safecallselvisoperator/SafeCallsElvisOperator.kt
@@ -0,0 +1,31 @@
+package safecallselvisoperator
+
+import nullabletypes.nullable
+
+fun main() {
+    safeCalls(null)
+    safeCalls("Rizka")
+
+    elvisOperator(null)
+    elvisOperator("topek")
+
+//    nonNullAssertion(null)
+//    nonNullAssertion("topeeekkkk")
+}
+
+fun safeCalls(text: String?) {
+//    println("jumlah karakter = ${text?.length}")
+    println(text?.length)
+}
+
+fun elvisOperator(text: String?) {
+    val textLength = text?.length ?: 9
+    println(textLength)
+}
+
+//fun nonNullAssertion(text: String?) {
+//    val textLength = text!!.length
+//    println(textLength)
+//}
+
+
