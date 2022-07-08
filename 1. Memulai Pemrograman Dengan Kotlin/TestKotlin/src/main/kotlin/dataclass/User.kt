35be629663f8b4e16ecc8719b66a99b04362121a
rizkakhrnnsa <khrnnsa.rizka@gmail.com>
2022-07-08 17:45:40 +0700

    Add copy modify data class.

--- 1. Memulai Pemrograman Dengan Kotlin/TestKotlin/src/main/kotlin/dataclass/User.kt
+++ 1. Memulai Pemrograman Dengan Kotlin/TestKotlin/src/main/kotlin/dataclass/User.kt
@@ -1,17 +1,20 @@
 package dataclass
 
-import jdk.nashorn.internal.runtime.NumberToString
-
 
 fun main() {
     val user = User("nrohmen", 17)
     val dataUser = DataUser("nrohmen", 17)
     val dataUser2 = DataUser("nrohmen", 17)
     val dataUser3 = DataUser("dimas", 24)
+    val dataUser4 = dataUser.copy()
+    val dataUser5 = dataUser.copy(age = 18)
+
     println(user)
     println(dataUser)
     println(dataUser.equals(dataUser2))
     println(dataUser.equals(dataUser3))
+    println(dataUser4)
+    println(dataUser5)
 
 }
 
