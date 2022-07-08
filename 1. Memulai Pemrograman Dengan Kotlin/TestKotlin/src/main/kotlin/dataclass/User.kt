9c2b5ab9f057124d16c31992cc91bc49e471585c
rizkakhrnnsa <khrnnsa.rizka@gmail.com>
2022-07-08 18:05:02 +0700

    Add destructuring declarations.

--- 1. Memulai Pemrograman Dengan Kotlin/TestKotlin/src/main/kotlin/dataclass/User.kt
+++ 1. Memulai Pemrograman Dengan Kotlin/TestKotlin/src/main/kotlin/dataclass/User.kt
@@ -9,13 +9,22 @@ fun main() {
     val dataUser4 = dataUser.copy()
     val dataUser5 = dataUser.copy(age = 18)
 
+    val dataUser6 = DataUser("nrohmen", 17)
+    val name = dataUser6.component1()
+    val age = dataUser6.component2()
+    val (nama, umur) = dataUser6
+
     println(user)
     println(dataUser)
     println(dataUser.equals(dataUser2))
     println(dataUser.equals(dataUser3))
     println(dataUser4)
     println(dataUser5)
+    println("my name is $name, I am $age years old")
+    println("my name is $nama, I am $umur years old")
 
+    val dataUser7 = DataUser2("nrohmen", 23)
+    dataUser7.intro()
 }
 
 class User(
@@ -31,3 +40,13 @@ data class DataUser(
     val name: String,
     val age: Int
 )
+
+data class DataUser2(
+    val name: String,
+    val age: Int
+) {
+    fun intro() {
+        println("My name is $name, I am $age years old")
+    }
+}
+
