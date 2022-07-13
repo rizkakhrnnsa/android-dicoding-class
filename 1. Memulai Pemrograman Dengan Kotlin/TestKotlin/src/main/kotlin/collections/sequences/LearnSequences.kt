d88a01941a62ada0779fa608dc81627ecc3a416b
rizkakhrnnsa <khrnnsa.rizka@gmail.com>
2022-07-13 21:23:05 +0700

    Add sequences .

---
+++ 1. Memulai Pemrograman Dengan Kotlin/TestKotlin/src/main/kotlin/collections/sequences/LearnSequences.kt
@@ -0,0 +1,17 @@
+package collections.sequences
+
+fun main() {
+    learnSequences()
+}
+
+fun learnSequences() {
+    val list = (1..1_000_000).toList()
+    // eager evaluation
+//    list.filter { it % 5 == 0 }.map { it * 2 }.forEach { println(it) }
+
+    // as sequences / lazy evaluation
+    list.asSequence().filter { it % 5 == 0 }.map { it * 2 }.forEach { println(it) }
+
+    val sequenceNumber = generateSequence(1) { it+1 }
+    sequenceNumber.take(5).forEach { println("$it") }
+}
